6dd8c7fbb4e450d444d7d9cdb712652a9ed1c886
NilmiSenevirathne <126900940+NilmiSenevirathne@users.noreply.github.com>
2025-02-11 10:05:29 +0530

    homepage creation

---
+++ frontend/src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+           <div className='home-container'> 
+                <h1>Welcome to <br/> Sathmaga Attendance Management System</h1>
+           </div>
+    </div>
+  )
+}
+
+export default Home
