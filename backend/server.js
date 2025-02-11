35c755972be62e7ad7145dc9b73471f8ed0b5985
gangaweerakkody <127628647+gangaweerakkody@users.noreply.github.com>
2025-02-11 09:42:23 +0530

    created database connection

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,37 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const cors = require("cors");
 const path = require("path");
 const app = express();
 //path.resolve()
-app.use(express.static(path.join(__dirname, "public")));
+
+// app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.json());
 
-const port = 5000;
-const db = mysql.createConnection({
+// create connnection
+const port = 8082;
+
+const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "SathmagaDB",
+  password: "1234",
+  database: "sathmagadb"
 });
 
+//test
+db.getConnection((err, connection)=> {
+  if(err){
+      console.error("Error connection to the database", err);
+      return;
+    }
+  else {
+    console.log("Connected to the MySQL database");
+    connection.release();   
+  }
+})
 
+//server start
 app.listen(port, () => {
     console.log(`listening on port ${port} `);
   });
